a07763aa1b839ebecbd50d8b8a045b1f78dea8aa
louis camara <imlouiscamara@gmail.com>
2026-07-09 21:20:04 +0000

    add initial implementation of conditions example in python basics 2

---
+++ python/python-course/02_python_basics_2/README.md
@@ -0,0 +1,5 @@
+# python basics 2
+
+## usage
+
+- use the python interactive window for a better experience
---
+++ python/python-course/02_python_basics_2/main.py
@@ -0,0 +1,14 @@
+# conditions
+is_old = True
+# is_old = False
+is_licenced = True
+# is_licenced = False
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+# elif is_licenced:
+    # print('you can drive now!')
+else:
+    print('you are not of age!')
+
+print('ok ok ok')
